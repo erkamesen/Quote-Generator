a9113bbfa6e51506ef3d2ec716310efd8c9927b4
Erkam Esen <120065120+erkamesen@users.noreply.github.com>
2023-01-24 20:19:46 +0300

    First commit

---
+++ Quote Generator - Flask/config.py
@@ -0,0 +1,7 @@
+
+
+SECRET_KEY = "thisisasecretkey"
+SQLALCHEMY_DATABASE_URI = 'sqlite:///quotes.db'
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+SIJAX_STATIC_PATH = '/static/script.js'
+SIJAX_JSON_URI = '/static/json2.js'
